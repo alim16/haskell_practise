59f36f1c2731e7413fd664bab84dc175568fa98b
mahamed ali <mohamed.tum@gmail.com>
2018-11-11 21:15:25 +0000

    after applicative instance

--- parser1.hs
+++ parser1.hs
@@ -18,10 +18,23 @@ item = P (\inp -> case inp of
 
 
 instance Functor Parser where
-    --fmap (a -> b) -> f a -> fb
+    --fmap :: (a -> b) -> f a -> fb
     fmap g p = P (\inp -> case parse p inp of
                             [] -> []
                             [(v,out)] -> [(g v,out)])
+
+instance Applicative Parser where
+    --pure :: a -> Parser a
+    pure v = P (\inp -> [(v,inp)])
+
+    -- <*> :: Parser (a -> b) -> Parser a -> Parser b
+    pg <*> px = P (\inp -> case parse pg inp of
+                    [] -> []
+                    [(g,out)] -> parse (fmap g px) out)
+
+three:: Parser (Char,Char)
+three = pure g <*> item <*> item <*> item
+        where g x y z = (x,z) 
 
 --main = do
         --a  <- getLine
